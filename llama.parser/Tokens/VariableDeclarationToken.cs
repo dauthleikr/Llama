72e9f3e4d3b01c55904405254a02bfbf0ea89d1b
dauthleikr <nino.neuw@gmail.com>
2019-07-16 17:37:40 +0200

    Add VariableDeclarationToken

---
+++ llama.parser/Tokens/VariableDeclarationToken.cs
@@ -0,0 +1,23 @@
+﻿namespace Llama.Parser.Tokens
+{
+    using Framework;
+
+    public class VariableDeclarationToken : TokenBase<VariableDeclarationToken>
+    {
+        public readonly IdentifierToken DeclarationName;
+        public readonly IdentifierToken DeclarationType;
+
+        public VariableDeclarationToken(IdentifierToken declarationType, IdentifierToken declarationName)
+        {
+            DeclarationType = declarationType;
+            DeclarationName = declarationName;
+        }
+
+        public override void WalkRecursive(ISourceWalker walker, bool codeChildrenOnly = true)
+        {
+            walker.Walk(this);
+            DeclarationType.WalkRecursive(walker, codeChildrenOnly);
+            DeclarationName.WalkRecursive(walker, codeChildrenOnly);
+        }
+    }
+}
