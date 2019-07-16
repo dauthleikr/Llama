94dd3e6db80cfa1b3dd3e13ae0b3428f04915ad6
dauthleikr <nino.neuw@gmail.com>
2019-07-16 17:59:45 +0200

    Add VariableDeclarationParser

---
+++ llama.parser/Parsers/VariableDeclarationParser.cs
@@ -0,0 +1,17 @@
+﻿namespace Llama.Parser.Parsers
+{
+    using Framework;
+    using Tokens;
+
+    internal class VariableDeclarationParser : ParserBase<VariableDeclarationToken>
+    {
+        public override ITokenizationResult<VariableDeclarationToken> TryReadToken(ISourceReader reader, IParseContext context, INonCodeParser nonCodeParser)
+        {
+            if (context.TryReadToken<IdentifierToken>(out var typeIdentifier) && context.TryReadToken<IdentifierToken>(out var nameIdentifier))
+                return new VariableDeclarationToken(typeIdentifier, nameIdentifier);
+            return ErrorExpectedToken(reader);
+        }
+
+        public override bool IsPlausible(ISourcePeeker reader, IParseContext context) => context.IsPlausible<IdentifierToken>();
+    }
+}
