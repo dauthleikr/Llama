dc6e8c8b4c7890e61279d869f59cd1ae52932451
dauthleikr <nino.neuw@gmail.com>
2019-08-12 13:13:44 +0200

    Begun working on the PE optional header struct

---
+++ Llama.PE/ExecutableKind.cs
@@ -0,0 +1,9 @@
+﻿namespace Llama.PE
+{
+    public enum ExecutableKind : ushort
+    {
+        ROM = 0x107,
+        PE32 = 0x10b,
+        PE32Plus = 0x20b
+    }
+}
---
+++ Llama.PE/PEOptionalHeader.cs
@@ -0,0 +1,10 @@
+﻿namespace Llama.PE
+{
+    using System.Runtime.InteropServices;
+
+    [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Ansi, Pack = 1)]
+    public struct PEOptionalHeader
+    {
+        public ExecutableKind ExecutableKind;
+    }
+}
