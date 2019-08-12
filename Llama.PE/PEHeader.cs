9deae559a740c8c48b21dc31a671e9a549a1a25a
dauthleikr <nino.neuw@gmail.com>
2019-08-12 13:12:37 +0200

    Added PE Header struct

---
+++ Llama.PE/PEHeader.cs
@@ -0,0 +1,18 @@
+﻿namespace Llama.PE
+{
+    using System.Reflection.PortableExecutable;
+    using System.Runtime.InteropServices;
+
+    [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Ansi, Pack = 1)]
+    public unsafe struct PEHeader
+    {
+        public fixed byte Magic[4];
+        public Machine Architecture;
+        public ushort NumberOfSections;
+        public uint CreationTimePOSIX;
+        public uint SymbolTableRVA;
+        public uint NumberOfSymbols;
+        public ushort OptionalHeaderSize;
+        public Characteristics Characteristics;
+    }
+}
