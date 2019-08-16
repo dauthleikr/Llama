339de7d6cac6a656ece3a4e5d394f07afeac55a8
dauthleikr <nino.neuw@gmail.com>
2019-08-16 16:25:06 +0200

    Started work on idata directory table

---
+++ Llama.PE/idata/ImportDirectoryEntry.cs
@@ -0,0 +1,14 @@
+﻿namespace Llama.PE.idata
+{
+    using System.Runtime.InteropServices;
+
+    [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Ansi, Pack = 1)]
+    public struct ImportDirectoryEntry
+    {
+        public uint ImportLookupTableRVA;
+        public uint CreationTimePOSIX;
+        public uint ForwarderChain;
+        public uint NameRVA;
+        public uint IAT_RVA;
+    }
+}
