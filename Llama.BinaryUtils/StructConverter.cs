be8c95ea30930b09e24b03afd6714d0254f2d1af
dauthleikr <nino.neuw@gmail.com>
2019-08-25 17:11:29 +0200

    One-line converting utility for structs and byte[]

---
+++ Llama.BinaryUtils/StructConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Llama.BinaryUtils
+{
+    using System.Runtime.InteropServices;
+
+    public static class StructConverter
+    {
+        public static byte[] GetBytes<T>(T item) where T : struct
+        {
+            var size = TypeSize<T>.Size;
+            var data = new byte[size];
+            MemoryMarshal.Write(data.AsSpan(), ref item);
+            return data;
+        }
+
+        public static T GetStruct<T>(byte[] data) where T : struct => MemoryMarshal.Read<T>(data.AsSpan());
+    }
+}
