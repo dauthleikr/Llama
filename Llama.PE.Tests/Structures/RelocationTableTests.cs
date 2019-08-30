235a345ff21b5305d912ea4bf28055e541bfa233
dauthleikr <nino.neuw@gmail.com>
2019-08-30 22:20:54 +0200

    Added unit test for base relocation table

---
+++ Llama.PE.Tests/Structures/RelocationTableTests.cs
@@ -0,0 +1,37 @@
+﻿namespace Llama.PE.Tests.Structures
+{
+    using System.IO;
+    using System.Linq;
+    using BinaryUtils;
+    using NUnit.Framework;
+    using PE.Structures.Sections.Reloc;
+
+    internal class RelocationTableTests : TestsUsingHeaders
+    {
+        public RelocationTableTests() : base(File.ReadAllBytes("test.exe")) { }
+
+        [Test]
+        public void CanParseHeaderAndEntries()
+        {
+            var relocRVA = OptionalHeader.DataDirectories.BaseRelocationTable.VirtualAddress;
+            var relocSection = SectionHeaders.First(sec => sec.NameString.StartsWith(".reloc"));
+            var startFileOffset = relocSection.PointerToRawData + relocRVA - relocSection.VirtualAddress;
+            var reader = new ArrayStructReaderWriter(TestFile)
+            {
+                Offset = startFileOffset
+            };
+
+            var blockHeader = reader.Read<BaseRelocationBlockHeader>();
+            var noEntries = (blockHeader.BlockSize - 8) / 2;
+            var firstBlockEntry = reader.Read<BaseRelocationBlockEntry>();
+            var secondBlockEntry = reader.Read<BaseRelocationBlockEntry>();
+
+            Assert.AreEqual(0x19000, blockHeader.PageRVA);
+            Assert.AreEqual(12, noEntries);
+            Assert.AreEqual(0xA110, firstBlockEntry.Entry);
+            Assert.AreEqual(0x110, firstBlockEntry.Offset);
+            Assert.AreEqual(BaseRelocationType.ImageRelBasedDir64, firstBlockEntry.Type);
+            Assert.AreEqual(0xA440, secondBlockEntry.Entry);
+        }
+    }
+}
