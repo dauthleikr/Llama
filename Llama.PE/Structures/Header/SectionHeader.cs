f3499f4e62f7804126678bc0d4c1cc1b2d1c6d68
dauthleikr <nino.neuw@gmail.com>
2019-08-30 15:29:43 +0200

    SectionHeader: Implement IEquatable

--- Llama.PE/Structures/Header/SectionHeader.cs
+++ Llama.PE/Structures/Header/SectionHeader.cs
@@ -6,7 +6,7 @@
     using System.Text;
 
     [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Ansi, Pack = 1)]
-    public struct SectionHeader
+    public struct SectionHeader : IEquatable<SectionHeader>
     {
         public const int NameSize = 8;
 
@@ -35,5 +35,41 @@
         public SectionCharacteristics Characteristics;
 
         public string NameString => Encoding.ASCII.GetString(BitConverter.GetBytes(Name));
+
+        public bool Equals(SectionHeader other) =>
+            Name == other.Name &&
+            VirtualSize == other.VirtualSize &&
+            VirtualAddress == other.VirtualAddress &&
+            SizeOfRawData == other.SizeOfRawData &&
+            PointerToRawData == other.PointerToRawData &&
+            PointerToRelocations == other.PointerToRelocations &&
+            PointerToLinenumbers == other.PointerToLinenumbers &&
+            NumberOfRelocations == other.NumberOfRelocations &&
+            NumberOfLinenumbers == other.NumberOfLinenumbers &&
+            Characteristics == other.Characteristics;
+
+        public override bool Equals(object obj) => obj is SectionHeader other && Equals(other);
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                var hashCode = Name.GetHashCode();
+                hashCode = (hashCode * 397) ^ (int)VirtualSize;
+                hashCode = (hashCode * 397) ^ (int)VirtualAddress;
+                hashCode = (hashCode * 397) ^ (int)SizeOfRawData;
+                hashCode = (hashCode * 397) ^ (int)PointerToRawData;
+                hashCode = (hashCode * 397) ^ (int)PointerToRelocations;
+                hashCode = (hashCode * 397) ^ (int)PointerToLinenumbers;
+                hashCode = (hashCode * 397) ^ NumberOfRelocations.GetHashCode();
+                hashCode = (hashCode * 397) ^ NumberOfLinenumbers.GetHashCode();
+                hashCode = (hashCode * 397) ^ (int)Characteristics;
+                return hashCode;
+            }
+        }
+
+        public static bool operator ==(SectionHeader left, SectionHeader right) => left.Equals(right);
+
+        public static bool operator !=(SectionHeader left, SectionHeader right) => !left.Equals(right);
     }
 }
