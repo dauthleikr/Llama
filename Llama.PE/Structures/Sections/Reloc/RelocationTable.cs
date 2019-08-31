3f8aded01d02d6cc83f3b274f02c5577f5f0a32c
dauthleikr <nino.neuw@gmail.com>
2019-08-31 19:10:51 +0200

    Added full Relocation table as class

---
+++ Llama.PE/Structures/Sections/Reloc/RelocationTable.cs
@@ -0,0 +1,13 @@
+﻿namespace Llama.PE.Structures.Sections.Reloc
+{
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class RelocationTable
+    {
+        public uint Size => (uint)Relocations.Sum(item => item.Key.BlockSize);
+        public readonly IReadOnlyDictionary<BaseRelocationBlockHeader, BaseRelocationBlockEntry[]> Relocations;
+
+        public RelocationTable(IReadOnlyDictionary<BaseRelocationBlockHeader, BaseRelocationBlockEntry[]> relocations) => Relocations = relocations;
+    }
+}
