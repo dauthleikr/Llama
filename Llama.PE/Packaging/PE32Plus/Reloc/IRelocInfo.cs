1ce8cfb2810cbf09f69975cb175c2e186ea23142
dauthleikr <nino.neuw@gmail.com>
2019-08-31 21:22:16 +0200

    Packaging: .reloc section

---
+++ Llama.PE/Packaging/PE32Plus/Reloc/IRelocInfo.cs
@@ -0,0 +1,9 @@
+﻿namespace Llama.PE.Packaging.PE32Plus.Reloc
+{
+    using Structures.Sections.Reloc;
+
+    internal interface IRelocInfo
+    {
+        RelocationTable Relocations { get; }
+    }
+}
---
+++ Llama.PE/Packaging/PE32Plus/Reloc/RelocPackager.cs
@@ -0,0 +1,24 @@
+﻿namespace Llama.PE.Packaging.PE32Plus.Reloc
+{
+    using System.Diagnostics;
+    using BinaryUtils;
+    using Converters;
+    using Structures.Sections.Reloc;
+
+    internal class RelocPackager : IPackage<IRelocInfo, IPackagingResult>
+    {
+        private readonly IPEWriter<RelocationTable> _relocWriter;
+
+        public RelocPackager(IPEWriter<RelocationTable> relocWriter) => _relocWriter = relocWriter;
+
+        public IPackagingResult Package(IRelocInfo param)
+        {
+            var rawData = new byte[param.Relocations.Size];
+            var writer = new ArrayStructReaderWriter(rawData);
+            _relocWriter.Write(writer, param.Relocations);
+
+            Debug.Assert((int)writer.Offset == rawData.Length - 1);
+            return new RawPackagingResult(rawData);
+        }
+    }
+}
