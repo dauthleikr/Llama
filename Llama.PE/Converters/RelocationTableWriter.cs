b208d96d1bbeb261f0c9d554d03b992c264e8592
dauthleikr <nino.neuw@gmail.com>
2019-08-31 20:59:55 +0200

    Converters: Added writer for full relocation table (untested)

---
+++ Llama.PE/Converters/RelocationTableWriter.cs
@@ -0,0 +1,17 @@
+﻿namespace Llama.PE.Converters
+{
+    using BinaryUtils;
+    using Structures.Sections.Reloc;
+
+    internal class RelocationTableWriter : IPEWriter<RelocationTable>
+    {
+        public void Write(IStructWriter writer, RelocationTable item)
+        {
+            foreach (var (header, entries) in item.Relocations)
+            {
+                writer.Write(header);
+                writer.WriteArray(entries);
+            }
+        }
+    }
+}
