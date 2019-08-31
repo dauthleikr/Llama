6898083dbbd73a3e64069fda7726da466f3b983c
dauthleikr <nino.neuw@gmail.com>
2019-08-31 19:11:31 +0200

    Converters: Added reader for full relocation table (untested)

---
+++ Llama.PE/Converters/RelocationTableReader.cs
@@ -0,0 +1,28 @@
+﻿namespace Llama.PE.Converters
+{
+    using System.Collections.Generic;
+    using BinaryUtils;
+    using Structures.Sections.Reloc;
+
+    internal class RelocationTableReader : IPEReader<RelocationTable>
+    {
+        public RelocationTable Read(IStructReader reader, IPE32PlusContext image)
+        {
+            var relocationTableSize = image.OptionalHeader.DataDirectories.BaseRelocationTable.Size;
+            var relocationTableEnd = reader.Offset + relocationTableSize;
+            var relocationTable = new Dictionary<BaseRelocationBlockHeader, BaseRelocationBlockEntry[]>();
+
+            while (reader.Offset < relocationTableEnd)
+            {
+                var header = reader.Read<BaseRelocationBlockHeader>();
+                var noEntries = (header.BlockSize - 8) / 2; // 8 bytes header, 2 bytes for each entry
+                var entries = new BaseRelocationBlockEntry[noEntries];
+                for (var i = 0; i < noEntries; i++)
+                    entries[i] = reader.Read<BaseRelocationBlockEntry>();
+                relocationTable[header] = entries;
+            }
+
+            return new RelocationTable(relocationTable);
+        }
+    }
+}
