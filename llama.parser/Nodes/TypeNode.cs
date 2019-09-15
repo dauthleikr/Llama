fef99b10e19422ff29a3695b3adda919f1593ef7
dauthleikr <nino.neuw@gmail.com>
2019-09-15 22:21:51 +0200

    Parser: Type

---
+++ llama.parser/Nodes/TypeNode.cs
@@ -0,0 +1,37 @@
+﻿namespace Llama.Parser.Nodes
+{
+    using System;
+    using System.ComponentModel;
+    using Lexer;
+
+    internal class TypeNode
+    {
+        public enum WrappingType
+        {
+            None,
+            PointerOf,
+            ArrayOf
+        }
+
+        public Token PrimitiveType { get; }
+        public WrappingType ChildRelation { get; }
+        public TypeNode Child { get; }
+
+        public TypeNode(Token primitiveType)
+        {
+            if (primitiveType.Kind != TokenKind.PrimitiveType)
+                throw new ArgumentException($"{nameof(primitiveType)} has to be of kind: {nameof(TokenKind.PrimitiveType)}");
+
+            PrimitiveType = primitiveType;
+        }
+
+        public TypeNode(TypeNode underlying, WrappingType wrappingType)
+        {
+            if (!Enum.IsDefined(typeof(WrappingType), wrappingType) || wrappingType == WrappingType.None)
+                throw new InvalidEnumArgumentException(nameof(wrappingType), (int)wrappingType, typeof(WrappingType));
+
+            Child = underlying ?? throw new ArgumentNullException(nameof(underlying));
+            ChildRelation = wrappingType;
+        }
+    }
+}
---
+++ llama.parser/Parsers/TypeParser.cs
@@ -0,0 +1,29 @@
+﻿namespace Llama.Parser.Parsers
+{
+    using Lexer;
+    using Nodes;
+
+    internal class TypeParser : IParse<TypeNode>
+    {
+        public TypeNode Read(IParseContext context)
+        {
+            var type = new TypeNode(context.ReadOrPanic(TokenKind.PrimitiveType));
+
+            while (true)
+                switch (context.NextCodeToken.Kind)
+                {
+                    case TokenKind.Pointer:
+                        context.ReadCodeToken();
+                        type = new TypeNode(type, TypeNode.WrappingType.PointerOf);
+                        break;
+                    case TokenKind.OpenSquareBracket:
+                        context.ReadCodeToken();
+                        context.ReadOrPanic(TokenKind.CloseSquareBracket);
+                        type = new TypeNode(type, TypeNode.WrappingType.ArrayOf);
+                        break;
+                    default:
+                        return type;
+                }
+        }
+    }
+}
