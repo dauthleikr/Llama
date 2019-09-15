0dc34a0f153b94486d62ad713bead915fcc6a314
dauthleikr <nino.neuw@gmail.com>
2019-09-15 18:33:21 +0200

    Parser: Use abstractions

--- llama.parser/Token.cs
+++ llama.parser/Token.cs
@@ -1,10 +1,12 @@
 ﻿namespace Llama.Parser
 {
-    public readonly struct Token
+    using Abstractions;
+
+    public readonly struct Token : IToken<TokenKind>
     {
-        public readonly TokenKind Kind;
         public readonly string RawText;
 
+        public TokenKind Kind { get; }
         public bool IsTrivia => Kind == TokenKind.WhitespaceOrControl || Kind == TokenKind.LineComment || Kind == TokenKind.BlockComment;
 
         public Token(TokenKind kind, string rawText)
