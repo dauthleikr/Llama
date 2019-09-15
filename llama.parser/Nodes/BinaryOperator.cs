c116bdf626a9f60e1f8bf0477bd2bc8f526ee6d7
dauthleikr <nino.neuw@gmail.com>
2019-09-15 22:38:30 +0200

    Parser: BinaryOperator

---
+++ llama.parser/Nodes/BinaryOperator.cs
@@ -0,0 +1,11 @@
+﻿namespace Llama.Parser.Nodes
+{
+    using Lexer;
+
+    internal class BinaryOperator
+    {
+        public Token Operator { get; }
+
+        public BinaryOperator(Token @operator) => Operator = @operator;
+    }
+}
---
+++ llama.parser/Parsers/BinaryOperatorParser.cs
@@ -0,0 +1,21 @@
+﻿namespace Llama.Parser.Parsers
+{
+    using Lexer;
+    using Nodes;
+
+    internal class BinaryOperatorParser : IParse<BinaryOperator>
+    {
+        public BinaryOperator Read(IParseContext context)
+        {
+            switch (context.NextCodeToken.Kind)
+            {
+                case TokenKind.Plus:
+                case TokenKind.Minus:
+                    return new BinaryOperator(context.ReadCodeToken());
+                default:
+                    context.Panic($"Expected {nameof(BinaryOperator)}");
+                    return null;
+            }
+        }
+    }
+}
