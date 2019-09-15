8255e40627319748e871d9efc7e583b525ee23b3
dauthleikr <nino.neuw@gmail.com>
2019-09-15 18:56:25 +0200

    Parser: Implement ParseContext

---
+++ llama.parser/ParseContext.cs
@@ -0,0 +1,35 @@
+﻿namespace Llama.Parser
+{
+    using Abstractions;
+
+    internal class ParseContext : IParseContext<TokenKind>
+    {
+        public IToken<TokenKind> NextCodeToken { get; private set; }
+        private readonly Lexer _lexer;
+        private readonly IParseStore _parseStore;
+        private readonly string _source;
+        private int _sourcePosition;
+
+        public ParseContext(IParseStore parseStore, Lexer lexer, string source)
+        {
+            _parseStore = parseStore;
+            _lexer = lexer;
+            _source = source;
+
+            PrepareNextToken();
+        }
+
+        public IToken<TokenKind> ReadCodeToken()
+        {
+            var token = NextCodeToken;
+            PrepareNextToken();
+            return token;
+        }
+
+        public TNode ReadNode<TNode>() => _parseStore.GetStrategyFor<TNode>().Read(this);
+
+        public void Panic(string message) => throw new ParserException(message);
+
+        private void PrepareNextToken() => NextCodeToken = _lexer.NextNonTriviaToken(_source, ref _sourcePosition);
+    }
+}
