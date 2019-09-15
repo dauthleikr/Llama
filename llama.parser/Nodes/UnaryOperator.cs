163f428ad40577f469d4191c5aa861bfa47b6d6a
dauthleikr <nino.neuw@gmail.com>
2019-09-15 22:49:59 +0200

    Parser: Unary operator

---
+++ llama.parser/Nodes/UnaryOperator.cs
@@ -0,0 +1,11 @@
+﻿namespace Llama.Parser.Nodes
+{
+    using Lexer;
+
+    internal class UnaryOperator
+    {
+        public Token Operator { get; }
+
+        public UnaryOperator(Token @operator) => Operator = @operator;
+    }
+}
---
+++ llama.parser/Parsers/UnaryOperatorParser.cs
@@ -0,0 +1,21 @@
+﻿namespace Llama.Parser.Parsers
+{
+    using Lexer;
+    using Nodes;
+
+    internal class UnaryOperatorParser : IParse<UnaryOperator>
+    {
+        public UnaryOperator Read(IParseContext context)
+        {
+            switch (context.NextCodeToken.Kind)
+            {
+                case TokenKind.AddressOf:
+                case TokenKind.Pointer:
+                    return new UnaryOperator(context.ReadCodeToken());
+                default:
+                    context.Panic($"Expected {nameof(UnaryOperatorParser)}");
+                    return null;
+            }
+        }
+    }
+}
