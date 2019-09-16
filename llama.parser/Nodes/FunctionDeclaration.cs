2b1403e1f3fc8f4d09bad9b563b40a3140b0e78f
dauthleikr <nino.neuw@gmail.com>
2019-09-16 18:43:36 +0200

    Parser: Function declaration

---
+++ llama.parser/Nodes/FunctionDeclaration.cs
@@ -0,0 +1,30 @@
+﻿namespace Llama.Parser.Nodes
+{
+    using Lexer;
+
+    internal class FunctionDeclaration
+    {
+        public class Parameter
+        {
+            public Type ParameterType { get; }
+            public Token ParameterIdentifier { get; }
+
+            public Parameter(Type parameterType, Token parameterIdentifier)
+            {
+                ParameterType = parameterType;
+                ParameterIdentifier = parameterIdentifier;
+            }
+        }
+
+        public Type ReturnType { get; }
+        public Token Identifier { get; }
+        public Parameter[] Parameters { get; }
+
+        public FunctionDeclaration(Type returnType, Token identifier, params Parameter[] parameters)
+        {
+            ReturnType = returnType;
+            Identifier = identifier;
+            Parameters = parameters;
+        }
+    }
+}
---
+++ llama.parser/Parsers/FunctionDeclarationParser.cs
@@ -0,0 +1,28 @@
+﻿namespace Llama.Parser.Parsers
+{
+    using System.Collections.Generic;
+    using Lexer;
+    using Nodes;
+
+    internal class FunctionDeclarationParser : IParse<FunctionDeclaration>
+    {
+        public FunctionDeclaration Read(IParseContext context)
+        {
+            var returnType = context.ReadNode<Type>();
+            var identifier = context.ReadOrPanic(TokenKind.Identifier);
+            context.ReadOrPanic(TokenKind.OpenParanthesis);
+            var parameters = new List<FunctionDeclaration.Parameter>();
+            while (context.NextCodeToken.Kind != TokenKind.CloseParanthesis)
+            {
+                var parameterType = context.ReadNode<Type>();
+                var parameterIdentifier = context.ReadOrPanic(TokenKind.Identifier);
+                if (context.NextCodeToken.Kind == TokenKind.Comma)
+                    context.ReadCodeToken();
+                parameters.Add(new FunctionDeclaration.Parameter(parameterType, parameterIdentifier));
+            }
+
+            context.ReadOrPanic(TokenKind.CloseParanthesis);
+            return new FunctionDeclaration(returnType, identifier, parameters.ToArray());
+        }
+    }
+}
