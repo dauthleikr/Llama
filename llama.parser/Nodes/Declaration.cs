afedc1184a69c09dffb997e9ca479efcd14bb5ba
dauthleikr <nino.neuw@gmail.com>
2019-09-16 17:49:36 +0200

    Parser: Declarations

---
+++ llama.parser/Nodes/Declaration.cs
@@ -0,0 +1,18 @@
+﻿namespace Llama.Parser.Nodes
+{
+    using Lexer;
+
+    internal class Declaration
+    {
+        public Type Type { get; }
+        public Token Identifier { get; }
+        public IExpression InitialValue { get; }
+
+        public Declaration(Type type, Token identifier, IExpression initialValue = null)
+        {
+            Type = type;
+            Identifier = identifier;
+            InitialValue = initialValue;
+        }
+    }
+}
---
+++ llama.parser/Parsers/DeclarationParser.cs
@@ -0,0 +1,22 @@
+﻿namespace Llama.Parser.Parsers
+{
+    using Lexer;
+    using Nodes;
+
+    internal class DeclarationParser : IParse<Declaration>
+    {
+        public Declaration Read(IParseContext context)
+        {
+            var type = context.ReadNode<Type>();
+            var name = context.ReadOrPanic(TokenKind.Identifier);
+
+            if (context.NextCodeToken.Kind == TokenKind.Assignment)
+            {
+                context.ReadOrPanic(TokenKind.Assignment);
+                return new Declaration(type, name, context.ReadNode<IExpression>());
+            }
+
+            return new Declaration(type, name);
+        }
+    }
+}
