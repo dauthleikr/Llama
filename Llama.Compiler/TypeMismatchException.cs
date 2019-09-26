1857f26a318cace39444139bc5253ed2e9627952
dauthleikr <nino.neuw@gmail.com>
2019-09-26 11:41:41 +0200

    Compiler: Register can be either float or int, added assertations

---
+++ Llama.Compiler/TypeMismatchException.cs
@@ -0,0 +1,9 @@
+﻿namespace Llama.Compiler
+{
+    using System;
+
+    internal class TypeMismatchException : Exception
+    {
+        public TypeMismatchException(string expected, string actual) : base($"Type mismatch: Expected {expected} but got {actual}") { }
+    }
+}
