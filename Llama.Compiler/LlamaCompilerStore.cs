86e02fee880c24f141d04c0c9dee588f4a7515a2
dauthleikr <nino.neuw@gmail.com>
2019-09-26 11:11:48 +0200

    Compiler: LlamaCompilerStore

---
+++ Llama.Compiler/LlamaCompilerStore.cs
@@ -0,0 +1,28 @@
+﻿namespace Llama.Compiler
+{
+    using Parser.Nodes;
+
+    public sealed class LlamaCompilerStore : ICompilerStore
+    {
+        private struct ExpressionCompilerStore<T> where T : IExpression
+        {
+            public static ICompileExpressions<T> Compiler;
+        }
+
+        private struct StatementCompilerStore<T> where T : IStatement
+        {
+            public static ICompileStatements<T> Compiler;
+        }
+
+        public static LlamaCompilerStore Instance { get; } = new LlamaCompilerStore();
+
+        private LlamaCompilerStore()
+        {
+            // todo
+        }
+
+        public ICompileExpressions<T> GetExpressionCompiler<T>() where T : IExpression => ExpressionCompilerStore<T>.Compiler;
+
+        public ICompileStatements<T> GetStatementCompiler<T>() where T : IStatement => StatementCompilerStore<T>.Compiler;
+    }
+}
