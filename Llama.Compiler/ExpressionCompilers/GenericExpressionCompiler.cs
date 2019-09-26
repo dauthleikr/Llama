72252e09323129608793430d7a9fd7403ae1615c
dauthleikr <nino.neuw@gmail.com>
2019-09-26 12:33:20 +0200

    Compiler: Generic expression compiler

---
+++ Llama.Compiler/ExpressionCompilers/GenericExpressionCompiler.cs
@@ -0,0 +1,36 @@
+﻿namespace Llama.Compiler.ExpressionCompilers
+{
+    using Parser.Nodes;
+    using spit;
+
+    internal class GenericExpressionCompiler : ICompileExpressions<IExpression>
+    {
+        public void Compile(
+            IExpression expression,
+            Register target,
+            CodeGen codeGen,
+            IFunctionContext function,
+            IAddressFixer addressFixer,
+            ICompilationContext context
+        )
+        {
+            switch (expression)
+            {
+                case ArrayAccessExpression arrayAccessExpression:
+                    break;
+                case ArrayAllocationExpression arrayAllocationExpression:
+                    break;
+                case AtomicExpression atomicExpression:
+                    break;
+                case BinaryOperatorExpression binaryOperatorExpression:
+                    break;
+                case MethodCallExpression methodCallExpression:
+                    break;
+                case TypeCastExpression typeCastExpression:
+                    break;
+                case UnaryOperatorExpression unaryOperatorExpression:
+                    break;
+            }
+        }
+    }
+}
