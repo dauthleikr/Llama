135767e52c47781ad15307c1bc7add197df63060
dauthleikr <nino.neuw@gmail.com>
2019-09-26 10:58:38 +0200

    Compiler: Layout of general structure

---
+++ Llama.Compiler/ICompileStatements.cs
@@ -0,0 +1,9 @@
+﻿namespace Llama.Compiler
+{
+    using Parser.Nodes;
+
+    internal interface ICompileStatements<in T> where T : IStatement
+    {
+        void Compile(T statement, IFunctionContext function, IAddressFixer addressFixer);
+    }
+}
---
+++ Llama.Compiler/ICompilerStore.cs
@@ -0,0 +1,10 @@
+﻿namespace Llama.Compiler
+{
+    using Parser.Nodes;
+
+    internal interface ICompilerStore
+    {
+        ICompileExpressions<T> GetExpressionCompiler<T>() where T : IExpression;
+        ICompileStatements<T> GetStatementCompiler<T>() where T : IStatement;
+    }
+}
