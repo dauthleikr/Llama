5e3912738faff08eebeda3e8de4d93479ca73433
dauthleikr <nino.neuw@gmail.com>
2019-09-26 12:32:33 +0200

    Compiler: Add magic constants

---
+++ Llama.Compiler/Constants.cs
@@ -0,0 +1,10 @@
+﻿namespace Llama.Compiler
+{
+    using System;
+
+    internal static class Constants
+    {
+        public const int DummyOffsetInt = int.MaxValue;
+        public static readonly string HeapHandle = Guid.NewGuid().ToString();
+    }
+}
