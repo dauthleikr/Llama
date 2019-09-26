e1c863569b527b950531e04911baf8dc7d1bf894
dauthleikr <nino.neuw@gmail.com>
2019-09-26 10:59:02 +0200

    Compiler: FunctionContext

---
+++ Llama.Compiler/FunctionContext.cs
@@ -0,0 +1,18 @@
+﻿namespace Llama.Compiler
+{
+    using System;
+
+    internal class FunctionContext : IFunctionContext
+    {
+        private readonly int _calleeParameterSpace;
+        private readonly string[] _locals;
+
+        public FunctionContext(string[] locals, int calleeParameterSpace)
+        {
+            _locals = locals;
+            _calleeParameterSpace = calleeParameterSpace;
+        }
+
+        public int GetLocalOffset(string identifier) => Array.IndexOf(_locals, identifier) * 8 + _calleeParameterSpace;
+    }
+}
