d896c39d3f6e2bc9db4e8c1d4d20e76e9d453e7b
dauthleikr <nino.neuw@gmail.com>
2019-09-26 11:41:41 +0200

    Compiler: Register can be either float or int, added assertations

--- Llama.Compiler/Register.cs
+++ Llama.Compiler/Register.cs
@@ -7,12 +7,42 @@
         public readonly Register64 IntegerRegister;
         public readonly XmmRegister FloatRegister;
         public readonly bool IsIntegerRegister;
+        public readonly bool EitherRegisterIsFine;
 
-        public Register(Register64 integerRegister, XmmRegister floatRegister, bool isIntegerRegister)
+        public Register(Register64 integerRegister)
+        {
+            IntegerRegister = integerRegister;
+            FloatRegister = XmmRegister.XMM0;
+            IsIntegerRegister = true;
+            EitherRegisterIsFine = false;
+        }
+
+        public Register(XmmRegister floatRegister)
+        {
+            FloatRegister = floatRegister;
+            IntegerRegister = Register64.RAX;
+            IsIntegerRegister = false;
+            EitherRegisterIsFine = false;
+        }
+
+        public Register(Register64 integerRegister, XmmRegister floatRegister)
         {
             IntegerRegister = integerRegister;
             FloatRegister = floatRegister;
-            IsIntegerRegister = isIntegerRegister;
+            IsIntegerRegister = false;
+            EitherRegisterIsFine = true;
+        }
+
+        public void AssertIsInteger()
+        {
+            if (!EitherRegisterIsFine && !IsIntegerRegister)
+                throw new TypeMismatchException("integer", "floating point");
+        }
+
+        public void AssertIsFloat()
+        {
+            if (!EitherRegisterIsFine && IsIntegerRegister)
+                throw new TypeMismatchException("floating point", "integer");
         }
     }
 }
