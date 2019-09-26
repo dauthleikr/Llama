0639c3831954c57841eb8c7d217bdb292db6f795
dauthleikr <nino.neuw@gmail.com>
2019-09-26 11:54:40 +0200

    Compiler: IAddressFixer assigns an identifier to data

--- Llama.Compiler/IAddressFixer.cs
+++ Llama.Compiler/IAddressFixer.cs
@@ -5,7 +5,8 @@
     public interface IAddressFixer
     {
         void FixIATEntryOffset(CodeGen generator, string library, string function);
-        void FixConstantDataOffset(CodeGen generator, byte[] data);
+        void FixConstantDataOffset(CodeGen generator, string identifier, byte[] data);
+        void FixConstantDataOffset(CodeGen generator, string identifier);
         void FixFunctionOffset(CodeGen generator, string identifier);
         void FixFunctionAddress(CodeGen generator, string identifier);
     }
