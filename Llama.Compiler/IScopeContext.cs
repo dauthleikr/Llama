9385383b4e3b9ca5b529f45889e748738a63c301
dauthleikr <nino.neuw@gmail.com>
2019-09-27 22:02:36 +0200

    Compiler: FunctionContext -> ScopeContext

---
+++ Llama.Compiler/IScopeContext.cs
@@ -0,0 +1,12 @@
+﻿namespace Llama.Compiler
+{
+    using Parser.Nodes;
+
+    public interface IScopeContext
+    {
+        int TotalFunctionStackSpace { get; }
+        int GetLocalOffset(string identifier);
+        Type GetLocalType(string identifier);
+        bool HasLocal(string identifier);
+    }
+}
--- Llama.Compiler/ScopeContext.cs
+++ Llama.Compiler/ScopeContext.cs
@@ -2,13 +2,15 @@
 {
     using System;
     using System.Diagnostics;
+    using System.Linq;
 
-    public class FunctionContext : IFunctionContext
+    public class ScopeContext : IScopeContext
     {
+        public int TotalFunctionStackSpace { get; }
         private readonly int _calleeParameterSpace;
         private readonly string[] _locals;
 
-        public FunctionContext(string[] locals, int calleeParameterSpace, int totalFunctionStackSpace)
+        public ScopeContext(string[] locals, int calleeParameterSpace, int totalFunctionStackSpace)
         {
             if (locals == null)
                 throw new ArgumentNullException(nameof(locals));
@@ -19,7 +21,14 @@
             TotalFunctionStackSpace = totalFunctionStackSpace;
         }
 
-        public int TotalFunctionStackSpace { get; }
-        public int GetLocalOffset(string identifier) => Array.IndexOf(_locals, identifier) * 8 + _calleeParameterSpace;
+        public int GetLocalOffset(string identifier)
+        {
+            var index = Array.IndexOf(_locals, identifier);
+            if (index < 0)
+                return index;
+            return index * 8 + _calleeParameterSpace;
+        }
+
+        public bool HasLocal(string identifier) => _locals.Contains(identifier);
     }
 }
