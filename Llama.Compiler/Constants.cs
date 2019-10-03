0f523198a09ca2ce3221991d8b39e9f96c4780b8
dauthleikr <nino.neuw@gmail.com>
2019-10-03 17:28:21 +0200

    Compiler: Array Allocations; Add long type to constants

--- Llama.Compiler/Constants.cs
+++ Llama.Compiler/Constants.cs
@@ -1,11 +1,13 @@
 ﻿namespace Llama.Compiler
 {
     using System;
+    using Type = Parser.Nodes.Type;
 
     internal static class Constants
     {
         public const int DummyOffsetInt = int.MaxValue;
         public const long DummyAddress = long.MaxValue;
         public static readonly string HeapHandle = Guid.NewGuid().ToString();
+        public static readonly Type LongType = new Type("long");
     }
 }
--- Llama.Compiler/ExpressionCompilers/ArrayAllocationCompiler.cs
+++ Llama.Compiler/ExpressionCompilers/ArrayAllocationCompiler.cs
@@ -5,25 +5,33 @@
 
     internal class ArrayAllocationCompiler : ICompileExpressions<ArrayAllocationExpression>
     {
-        public void Compile(
+        public Type Compile(
             ArrayAllocationExpression expression,
             Register target,
             CodeGen codeGen,
-            IFunctionContext function,
+            IScopeContext scope,
             IAddressFixer addressFixer,
             ICompilationContext context
         )
         {
-            context.CompileExpression(expression.Count, codeGen, Register64.RAX, function);
+            var indexType = context.CompileExpression(expression.Count, codeGen, Register64.RAX, scope);
+            Constants.LongType.AssertCanAssign(indexType);
+
+            // if the original value is less than 4 byte, the asm instructions do not guarantee that the upper part of the target register is cleared
+            if (indexType.SizeOf() < 4)
+                codeGen.And(Register64.RAX, (1 << (8 * indexType.SizeOf())) - 1);
 
             // Array allocation compiles to function call of HeapAlloc (kernel32)
             codeGen.MovFromDereferenced(Register64.RCX, Constants.DummyOffsetInt);
-            addressFixer.FixConstantDataOffset(codeGen, Constants.HeapHandle);
+            addressFixer.FixConstantDataOffset(codeGen, Constants.HeapHandle); // Parameter 1: DefaultHeapHandle
+
             codeGen.Xor(Register64.RDX, Register64.RDX);
-            codeGen.Add(Register64.RDX, (sbyte)(0x8 + 0x4)); // HEAP_ZERO_MEMORY + HEAP_GENERATE_EXCEPTIONS
+            codeGen.Add(Register64.RDX, (sbyte)(0x8 + 0x4)); // Parameter 2: HEAP_ZERO_MEMORY + HEAP_GENERATE_EXCEPTIONS
             codeGen.Imul(Register64.R8, Register64.RAX, (sbyte)expression.Type.SizeOf()); // todo: rework if adding structs
+
             codeGen.CallRelative(Constants.DummyOffsetInt);
             addressFixer.FixIATEntryOffset(codeGen, "kernel32.dll", "HeapAlloc");
+            return new Type(expression.Type, Type.WrappingType.ArrayOf);
         }
     }
 }
