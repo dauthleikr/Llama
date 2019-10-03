a71cc8a29c1a6177143d8cbe4741c6e1e8223fa6
dauthleikr <nino.neuw@gmail.com>
2019-10-03 20:02:45 +0200

    Parser: Type now IEquatable

--- Llama.Parser/Nodes/Type.cs
+++ Llama.Parser/Nodes/Type.cs
@@ -3,7 +3,7 @@
     using System;
     using System.ComponentModel;
 
-    public class Type
+    public class Type : IEquatable<Type>
     {
         public enum WrappingType
         {
@@ -35,5 +35,40 @@
                 WrappingType.PointerOf => $"{Child}*",
                 _ => throw new ArgumentOutOfRangeException(ChildRelation.ToString())
             };
+
+        public bool Equals(Type other)
+        {
+            if (ReferenceEquals(null, other))
+                return false;
+            if (ReferenceEquals(this, other))
+                return true;
+            return PrimitiveType == other.PrimitiveType && ChildRelation == other.ChildRelation && Equals(Child, other.Child);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj))
+                return false;
+            if (ReferenceEquals(this, obj))
+                return true;
+            if (obj.GetType() != this.GetType())
+                return false;
+            return Equals((Type)obj);
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                var hashCode = (PrimitiveType != null ? PrimitiveType.GetHashCode() : 0);
+                hashCode = (hashCode * 397) ^ (int)ChildRelation;
+                hashCode = (hashCode * 397) ^ (Child != null ? Child.GetHashCode() : 0);
+                return hashCode;
+            }
+        }
+
+        public static bool operator ==(Type left, Type right) => Equals(left, right);
+
+        public static bool operator !=(Type left, Type right) => !Equals(left, right);
     }
 }
