82578f7cc021752acf918cac479d9e38b71e2068
dauthleikr <nino.neuw@gmail.com>
2019-10-14 19:19:02 +0200

    Compiler: ForCompiler; Formatting adjustments to WhileCompiler

--- Llama.Compiler/StatementCompilers/ForCompiler.cs
+++ Llama.Compiler/StatementCompilers/ForCompiler.cs
@@ -4,10 +4,10 @@
     using Parser.Nodes;
     using spit;
 
-    internal class WhileCompiler : ICompileStatements<While>
+    internal class ForCompiler : ICompileStatements<For>
     {
         public void Compile(
-            While statement,
+            For statement,
             CodeGen codeGen,
             StorageManager storageManager,
             IScopeContext scope,
@@ -15,18 +15,22 @@
             ICompilationContext context
         )
         {
-            var startPos = codeGen.StreamPosition;
-            var preferredRegisterCondition = new PreferredRegister(Register64.RAX);
-            var whileConditionResult = context.CompileExpression(statement.Condition, codeGen, storageManager, preferredRegisterCondition, scope);
+            context.CompileStatement(statement.Variable, codeGen, storageManager, scope);
 
-            whileConditionResult.GenerateMoveTo(Register64.RAX, Constants.BoolType, codeGen, addressFixer);
+            var conditionStart = codeGen.StreamPosition;
+            var preferredRegisterCondition = new PreferredRegister(Register64.RAX);
+            var forConditionResult = context.CompileExpression(statement.Condition, codeGen, storageManager, preferredRegisterCondition, scope);
+
+            forConditionResult.GenerateMoveTo(Register64.RAX, Constants.BoolType, codeGen, addressFixer);
             codeGen.Test(Register32.EAX, Register32.EAX);
 
             var childContext = context.CreateChildContext();
             var bodyCodeGen = new CodeGen();
 
             childContext.CompileStatement(statement.Instruction.StatementAsBlock(), bodyCodeGen, storageManager, scope);
-            var offsetToStart = startPos - codeGen.StreamPosition - bodyCodeGen.GetDataSpan().Length;
+            childContext.CompileExpression(statement.Increment, bodyCodeGen, storageManager, new PreferredRegister(Register64.RAX), scope);
+
+            var offsetToStart = conditionStart - codeGen.StreamPosition - bodyCodeGen.GetDataSpan().Length;
             if (offsetToStart >= sbyte.MinValue)
                 bodyCodeGen.Jmp((sbyte)offsetToStart);
             else
