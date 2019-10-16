9d9b94595cd1257d2f7bba5dbe1d2c1e81850249
dauthleikr <nino.neuw@gmail.com>
2019-10-16 14:39:49 +0200

    Compiler.Cli: Initial version

---
+++ Llama.Compiler.Cli/LinkerFactory.cs
@@ -0,0 +1,9 @@
+﻿namespace Llama.Compiler.Cli
+{
+    using Linker;
+
+    internal class LinkerFactory : IFactory<IAddressFixer>
+    {
+        public IAddressFixer Create() => new Linker();
+    }
+}
