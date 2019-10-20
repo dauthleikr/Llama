ed0bcee53c04337e7c289fc0bdd17400da6cce9e
dauthleikr <nino.neuw@gmail.com>
2019-10-20 16:28:45 +0200

    Lexer: Fix precedence issues

--- Llama.Parser/Lexer/Lexer.cs
+++ Llama.Parser/Lexer/Lexer.cs
@@ -1,23 +1,67 @@
 ﻿namespace Llama.Parser.Lexer
 {
+    using System;
     using System.Collections.Generic;
     using System.Linq;
 
     public class Lexer
     {
-        private readonly ITokenize[] _tokenizes;
+        private readonly ITokenize[] _greedyTokenizers;
+        private readonly ITokenize[] _staticTokenizers;
 
-        public Lexer(IEnumerable<ITokenize> tokenizers) => _tokenizes = tokenizers.ToArray();
+        public Lexer(IEnumerable<ITokenize> staticTokenizers, IEnumerable<ITokenize> greedyTokenizers)
+        {
+            _greedyTokenizers = greedyTokenizers.ToArray();
+            _staticTokenizers = staticTokenizers.ToArray();
+        }
 
         public Token NextToken(string source, ref int position)
         {
             if (position >= source.Length)
                 return new Token(TokenKind.EndOfStream, string.Empty);
-            foreach (var tokenizer in _tokenizes)
-                if (tokenizer.TryRead(source, ref position, out var token))
+
+            // First, try to find a perfectly matching token
+            var internalPositionStatic = position;
+            Token staticResult = default;
+            foreach (var tokenizer in _staticTokenizers)
+            {
+                if (!tokenizer.TryRead(source, ref internalPositionStatic, out var token))
+                    continue;
+
+                staticResult = token;
+                break;
+            }
+
+            /*
+              Then, try to find a better one with a non-static implementation (e.g. RegEx)
+              This is done because an identifier called 'intToString' could be read as type 'int' 
+                and identifier 'ToString' because there are no rules that tokens need to be seperated somehow.
+              With this implementation, a greedy identifier-tokenizers would read 'intToString' as one, 
+                and it would be deemed better than 'int' based on token length
+             */
+            foreach (var tokenizer in _greedyTokenizers)
+            {
+                var internalPositionGreedy = position;
+                if (!tokenizer.TryRead(source, ref internalPositionGreedy, out var token))
+                    continue;
+
+                if (token.RawText.Length > (staticResult.RawText?.Length ?? 0))
+                {
+                    position = internalPositionGreedy;
                     return token;
-            throw new LexerException("No matching token");
+                }
+            }
+
+            if (staticResult != default)
+            {
+                position = internalPositionStatic;
+                return staticResult;
+            }
+
+            throw new LexerException($"No matching token: {TakeSome(source.Substring(position), 10)} ...");
         }
+
+        private static string TakeSome(string str, int maxChars) => str.Substring(0, Math.Min(str.Length, maxChars));
 
         public Token NextNonTriviaToken(string source, ref int position)
         {
--- Llama.Parser/Lexer/LexerBuilder.cs
+++ Llama.Parser/Lexer/LexerBuilder.cs
@@ -61,11 +61,12 @@
             }
         }
 
-        private readonly List<ITokenize> _tokenizers = new List<ITokenize>();
+        private readonly List<ITokenize> _greedyTokenizers = new List<ITokenize>();
+        private readonly List<ITokenize> _staticTokenizers = new List<ITokenize>();
 
-        public void AddStaticToken(TokenKind kind, string staticText) => _tokenizers.Add(new StaticTextTokenizer(staticText, kind));
-        public void AddRegexToken(TokenKind kind, Regex regex) => _tokenizers.Add(new RegExTokenizer(regex, kind));
-        public Lexer Build() => new Lexer(_tokenizers);
+        public void AddStaticToken(TokenKind kind, string staticText) => _staticTokenizers.Add(new StaticTextTokenizer(staticText, kind));
+        public void AddRegexToken(TokenKind kind, Regex regex) => _greedyTokenizers.Add(new RegExTokenizer(regex, kind));
+        public Lexer Build() => new Lexer(_staticTokenizers, _greedyTokenizers);
 
         public static Lexer BuildLlamaLexer()
         {
@@ -77,9 +78,6 @@
              * rework that logic sometimes.
              */
 
-            builder.AddRegexToken(TokenKind.LineComment, new Regex(@"//[^\n\r]*", RegexOptions.Compiled));
-            builder.AddRegexToken(TokenKind.BlockComment, new Regex(@"\/\*(\*(?!\/)|[^*])*\*\/", RegexOptions.Compiled));
-            builder.AddRegexToken(TokenKind.WhitespaceOrControl, new Regex(@"[\s\t\n\r]+", RegexOptions.Compiled));
             builder.AddStaticToken(TokenKind.Equals, "==");
             builder.AddStaticToken(TokenKind.NotEquals, "!=");
             builder.AddStaticToken(TokenKind.GreaterEquals, ">=");
@@ -112,12 +110,24 @@
             builder.AddStaticToken(TokenKind.While, "while");
             builder.AddStaticToken(TokenKind.For, "for");
             builder.AddStaticToken(TokenKind.Return, "return");
-            builder.AddRegexToken(TokenKind.PrimitiveType, new Regex("(void|int|long|short|byte|sbyte|cstr|float|double|bool)", RegexOptions.Compiled));
+            builder.AddStaticToken(TokenKind.PrimitiveType, "void");
+            builder.AddStaticToken(TokenKind.PrimitiveType, "int");
+            builder.AddStaticToken(TokenKind.PrimitiveType, "long");
+            builder.AddStaticToken(TokenKind.PrimitiveType, "short");
+            builder.AddStaticToken(TokenKind.PrimitiveType, "byte");
+            builder.AddStaticToken(TokenKind.PrimitiveType, "sbyte");
+            builder.AddStaticToken(TokenKind.PrimitiveType, "cstr");
+            builder.AddStaticToken(TokenKind.PrimitiveType, "float");
+            builder.AddStaticToken(TokenKind.PrimitiveType, "double");
+            builder.AddStaticToken(TokenKind.PrimitiveType, "bool");
             builder.AddRegexToken(TokenKind.StringLiteral, new Regex("\"[^\"]*\"", RegexOptions.Compiled));
-            builder.AddRegexToken(TokenKind.IntegerLiteral, new Regex("[0-9]+[0-9_]*", RegexOptions.Compiled));
             builder.AddRegexToken(TokenKind.FloatLiteral, new Regex(@"([0-9]+[0-9_]*)?\.[0-9]+[0-9_]*", RegexOptions.Compiled));
+            builder.AddRegexToken(TokenKind.IntegerLiteral, new Regex("[0-9]+[0-9_]*", RegexOptions.Compiled));
             builder.AddRegexToken(TokenKind.Identifier, new Regex("[_A-Za-z][_A-Za-z0-9]*", RegexOptions.Compiled));
-            
+            builder.AddRegexToken(TokenKind.LineComment, new Regex(@"//[^\n\r]*", RegexOptions.Compiled));
+            builder.AddRegexToken(TokenKind.BlockComment, new Regex(@"\/\*(\*(?!\/)|[^*])*\*\/", RegexOptions.Compiled));
+            builder.AddRegexToken(TokenKind.WhitespaceOrControl, new Regex(@"[\s\t\n\r]+", RegexOptions.Compiled));
+
             return builder.Build();
         }
     }
