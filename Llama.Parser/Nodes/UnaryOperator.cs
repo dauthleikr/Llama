9e3c4647083640b327e5b0d30bf1b7aba5c19c2e
dauthleikr <nino.neuw@gmail.com>
2019-10-20 12:46:24 +0200

    Grammar & Parser: unary operators

--- Llama.Parser/Nodes/UnaryOperator.cs
+++ Llama.Parser/Nodes/UnaryOperator.cs
@@ -2,12 +2,13 @@
 {
     using Lexer;
 
-    public class UnaryOperator
+    public class UnaryOperator : IExpression
     {
         public Token Operator { get; }
 
         public UnaryOperator(Token @operator) => Operator = @operator;
 
-        public static bool IsTokenKindValid(TokenKind kind) => kind == TokenKind.AddressOf || kind == TokenKind.Pointer;
+        public static bool IsTokenKindValid(TokenKind kind) =>
+            kind == TokenKind.AddressOf || kind == TokenKind.Pointer || kind == TokenKind.Minus || kind == TokenKind.Not;
     }
 }
